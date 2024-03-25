330100d0805c39720f44b477f293d97358e6ee4a
RiccardoSilvestri <r.silvestri04@gmail.com>
2024-03-25 12:53:06 +0100

    Bug fixed

--- JavaClient/src/main/java/com/example/javaclient/notes/Note.java
+++ JavaClient/src/main/java/com/example/javaclient/notes/Note.java
@@ -23,28 +23,17 @@ public class Note {
         newStage.setTitle("New Window");
         newStage.show();
 
-
         SendButton(newRoot);
 
         TextArea noteTextArea = (TextArea) newRoot.lookup("#noteTextArea");
-        ImportedNotes(noteTextArea);
-
-
-        // Ottieni riferimenti al Label e all'HBox
-        Label titoloLabel = (Label) newRoot.lookup("#titoloLabel");
         HBox bottoniHBox = (HBox) newRoot.lookup("#bottoniHBox");
+        ImportedNotes(noteTextArea, bottoniHBox);
 
-        // Imposta il titolo
+        Label titoloLabel = (Label) newRoot.lookup("#titoloLabel");
         titoloLabel.setText("Titolo Variabile");
-
-        // Aggiungi bottoni all'HBox
-        for (int i = 1; i <= 9; i++) {
-            Button button = new Button(String.valueOf(i));
-            bottoniHBox.getChildren().add(button);
-        }
+        noteTextArea.setText("benvenuto");
 
     }
-
 
     private static void SendButton(VBox newRoot) {
         HBox sendButtonHBox = new HBox();
@@ -54,20 +43,53 @@ public class Note {
         newRoot.getChildren().add(sendButtonHBox);
     }
 
+    private static void ImportedNotes(TextArea noteTextArea, HBox bottoniHBox) {
+        String jsonString = "[{\"Author\":\"Paolo\",\"Content\":\"Oggi sono molto contento\",\"Date\":\"16/11/2002\",\"Title\":\"Nuova\"},{\"Author\":\"Paolo\",\"Content\":\"Lecca lecca\",\"Date\":\"16/11/2002\",\"Title\":\"ruba\"},{\"Author\":\"Paolo\",\"Content\":\"Lello bello\",\"Date\":\"16/11/2002\",\"Title\":\"Rancido\"}]";
 
-    private static void ImportedNotes(TextArea noteTextArea) {
-        String jsonString = "[{\"Author\":\"Paolo\",\"Content\":\"Oggi sono molto contento\",\"Date\":\"16/11/2002\",\"Title\":\"Nuova\"}]";
+        ButtonIncrease(noteTextArea, bottoniHBox, jsonString);
+    }
+    
 
+    private static void ButtonIncrease(TextArea noteTextArea, HBox bottoniHBox, String jsonString) {
         try {
             JSONArray jsonArray = new JSONArray(jsonString);
-            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            int contentCount = 0; // Contatore per i "Content"
 
-            String content = jsonObject.getString("Content");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (jsonObject.has("Content")) {
+                    contentCount++; // Incrementa il contatore se c'è un "Content"
+                }
+            }
 
-            noteTextArea.setText(content);
+            Contentviewer(noteTextArea, bottoniHBox, contentCount, jsonArray);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private static void Contentviewer(TextArea noteTextArea, HBox bottoniHBox, int contentCount, JSONArray jsonArray) {
+        for (int i = 1; i <= contentCount; i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i - 1); // Ottieni l'oggetto JSON per il bottone corrente
+            String title = jsonObject.getString("Title"); // Ottieni il titolo dal JSON
+            Button button = new Button(title); // Crea il bottone con il titolo come testo
+
+            // Aggiungi un EventHandler per gestire la pressione del bottone
+            button.setOnAction(event -> {
+                String buttonText = ((Button) event.getSource()).getText(); // Ottieni il testo del bottone (il titolo)
+                for (int j = 0; j < jsonArray.length(); j++) {
+                    JSONObject noteObject = jsonArray.getJSONObject(j);
+                    if (noteObject.getString("Title").equals(buttonText)) {
+                        String content = noteObject.getString("Content"); // Ottieni il "Content" corrispondente al titolo
+                        noteTextArea.setText(content); // Imposta il testo del TextArea con il "Content" trovato
+                        break;
+                    }
+                }
+            });
+
+            bottoniHBox.getChildren().add(button);
+        }
+    }
+
 
 }
