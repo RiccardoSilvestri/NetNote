2434ad5e4d454c72ba78bb6e43d11ccbd7bc0272
fralnz <francesco@fragiolanza.eu>
2024-04-08 18:20:45 +0200

    kill all threads when closing newStage

--- JavaClient/src/main/java/com/example/javaclient/notes/Note.java
+++ JavaClient/src/main/java/com/example/javaclient/notes/Note.java
@@ -1,5 +1,6 @@
 package com.example.javaclient.notes;
 
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -48,6 +49,10 @@ public class Note {
         // Initialization of NoteManagement
         NoteManagement noteManagement = new NoteManagement(newRoot);
         noteManagement.initialize(client,newRoot,newStage, user);
+        newStage.setOnCloseRequest(event -> {
+            Platform.exit();
+            System.exit(0);
+        });
     }
 
 
