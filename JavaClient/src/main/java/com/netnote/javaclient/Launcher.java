1d0eb9b8bdb256cff34e440896f2060979fab945
fralnz <francesco@fragiolanza.eu>
2024-04-10 22:19:31 +0200

    maven

---
+++ JavaClient/src/main/java/com/netnote/javaclient/Launcher.java
@@ -0,0 +1,9 @@
+package com.netnote.javaclient;
+
+import static javafx.application.Application.launch;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainWindow.main(args);
+    }
+}
